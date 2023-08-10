06ba7f2bdddd1a09afbaf7737037a76d2b7c7fa3
vibha <vibha.p@elaunchinfotech.in>
2023-08-11 01:38:03 +0530

    variables

---
+++ DartConcepts/operators.dart
@@ -0,0 +1,9 @@
+void main(){
+  print(9/3);
+  print("addition of two numbers is ${9+3}");
+  print("multiplication of two numbers is ${9*3}");
+  print("subtraction of two numbers is ${9-3}");
+
+  //it will print 
+  print("8"*899);
+}
